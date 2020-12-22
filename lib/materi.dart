14abbbd9efed2e864b4ef6eb5b06face9d680cc6
Rinaldy <rinaldy@Rinaldys-MacBook-Pro.local>
2020-12-22 22:25:48 +0700

    latihan

--- lib/materi.dart
+++ lib/materi.dart
@@ -1,8 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class MateriPage extends StatelessWidget {
+class MateriPage extends StatefulWidget {
+  @override
+  _MateriPageState createState() => _MateriPageState();
+}
+
+class _MateriPageState extends State<MateriPage> {
+  Widget choiceButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: Text(
+          "Option 1",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+          maxLines: 1,
+        ),
+        highlightColor: Colors.indigo[700],
+        splashColor: Colors.indigo[700],
+        color: Colors.indigoAccent,
+        minWidth: 200,
+        height: 45,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+    return WillPopScope(
+      onWillPop: () {
+        return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Latihan"),
+            content: Text("Maaf anda tidak bisa kembali"),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Ok"))
+            ],
+          ),
+        );
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(15.0),
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "oooooooooooo",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  choiceButton(),
+                  choiceButton(),
+                  choiceButton(),
+                  choiceButton()
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.topCenter,
+                child: Center(
+                  child: Text(
+                    "30",
+                    style:
+                        TextStyle(fontSize: 35.0, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
